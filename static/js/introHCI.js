e61a0b3d3cecf0d87771ac060d9bd79df9881baa
Danlin <jiangdl0129@gmail.com>
2021-01-14 23:13:39 +0800

    final

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -3,7 +3,20 @@ $(document).ready(function() {
 });
 
 function initializePage() {
+    $("#testjs").on("click", function(){
+        alert("clicked");
+     }); 
     $('#testjs').click(function(e) {
             $('.jumbotron h1').text("Javascript is connected");
             });
+    
+    $("a.thumbnail").click(projectClick);
+}
+
+function projectClick(e) {
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    $(this).css("background-color", "#7fff00");
 }
